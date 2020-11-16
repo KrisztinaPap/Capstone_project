cf5f647e80f970b4d921f03d84dd4f5ae7afde37
L Graham <lgraham92@gmail.com>
2020-11-16 16:37:56 -0700

    Added recipe_category model

---
+++ backend/Models/recipe_category.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using FluentValidation;
+
+namespace Api.Models
+{
+
+  [Table("Recipe_Categories")]
+  public class recipe_category {
+
+    [Key]
+    [Column(TypeName = "int(10)")]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; private set; }
+    
+    [Required]
+    [Column(TypeName = "varchar(50)")]
+    public string Name { get; set; }
+  }
+}
