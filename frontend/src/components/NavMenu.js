a6e14145eea4dc9fe60b5ce1385f656412beed24
Krisztina Pap <krisztina.pap@outlook.com>
2020-11-18 14:22:49 -0700

    [F]style: Additional menu styling (hover)

--- frontend/src/components/NavMenu.js
+++ frontend/src/components/NavMenu.js
@@ -27,34 +27,34 @@ export class NavMenu extends Component {
         <Navbar className="navbar-expand-lg navbar-toggleable-lg ng-white border-bottom box-shadow mb-3 text-white bg-purple-500" light>
           <Container>
             <NavbarBrand tag={Link} to="/" className="text-white">PuddleJumpers App</NavbarBrand>              
-            <button onClick={this.toggleNavbar} className="mr-2 lg:hidden">
+            <button onClick={this.toggleNavbar} className="mr-2 lg:hidden hover:bg-purple-600">
                 <i class="fas fa-bars fa-lg"></i>
             </button>
             <Collapse className="d-lg-inline-flex flex-lg-row-reverse" isOpen={!this.state.collapsed} navbar>
               <ul className="navbar-nav flex-grow justify-end">
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/">Home</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/">Home</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/dashboard">Dashboard</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/dashboard">Dashboard</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/login">Login</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/login">Login</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/signup">Signup</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/signup">Signup</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/profile">Edit Profile</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/profile">Edit Profile</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/recipes">Recipes</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/recipes">Recipes</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/recipe">Recipe</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/recipe">Recipe</NavLink>
                 </NavItem>
                 <NavItem>
-                  <NavLink tag={Link} className="text-white" to="/add-recipe">Add Recipe</NavLink>
+                  <NavLink tag={Link} className="text-white hover:bg-purple-600 pl-2 focus:bg-purple-600" to="/add-recipe">Add Recipe</NavLink>
                 </NavItem>
               </ul>
             </Collapse>
