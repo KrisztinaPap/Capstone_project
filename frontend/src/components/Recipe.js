1680100f7c68250bc31a305ca2e6cb0186e89b6e
L Graham <lgraham92@gmail.com>
2020-11-19 15:17:19 -0700

    [F]Recipe/Recipes Pages Started - Still incomplete; Added 'exact' to link for dynamic pages; Added dynamic page redirection;

--- frontend/src/components/Recipe.js
+++ frontend/src/components/Recipe.js
@@ -1,8 +1,48 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Recipe = () => {
-  return (
-    <h1>Recipe page</h1>
+function Recipe({match}){
+
+  const [myRecipe, setMyRecipe] = useState([]);
+
+  useEffect(() => {
+    fetchRecipe();
+  }, []);
+
+  async function fetchRecipe(){
+    // TODO: Change below page to render based on page loaded instead of hard coded to '-1'. Hard coded just for initial design.
+    const response = await axios.get(`api/recipes/-1`);
+    setMyRecipe(response.data);
+    console.log(response.data);
+    console.log(response.data.ingredients[0])
+  }
+
+  // TODO: Render Stuff:
+  // Placeholder image change to myRecipe.image once images are stored in DB.
+  // Loop over ingredients list and display all ingredients instead of hard coding them
+  return(
+    <>
+      <p><img src={"https://designshack.net/wp-content/uploads/placehold.jpg"} /></p>
+      <h1>{myRecipe.name}</h1>
+      <h4>Servings: {myRecipe.servings}</h4>
+      <h4>Prep Time: {myRecipe.prepTime}</h4>
+
+      <h4>Ingredients:</h4>
+
+      <h4>Macros:</h4>
+      <p>Calories: {myRecipe.calories}</p>
+      <p>Fat {myRecipe.fat}g</p>
+      <p>Protein {myRecipe.protein}g</p>
+      <p>Carbs {myRecipe.carbohydrates}g</p>
+
+      <h4>Ingredients</h4>
+
+
+      <h4>Notes:</h4>
+      <p>{myRecipe.notes}</p>
+
+    </>
   );
 }
 
--- frontend/src/components/Recipes.js
+++ frontend/src/components/Recipes.js
@@ -1,9 +1,76 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
 
-const Recipes = () => {
+
+function Recipes() {
+
+  useEffect(() => {
+    populateRecipes();
+  }, []);
+
+  const [recipes, setRecipes] = useState([]);
+
+  async function populateRecipes(){
+    const response = await axios.get('api/recipes')
+    setRecipes(response.data);
+  }
+
+
   return (
-    <h1>Recipes page</h1>
+    <>
+      <main>
+        <section>
+          <h2>
+            <i className="fas fa-drumstick-bite"></i>
+            Recipes
+          </h2>
+          <table>
+            <thead>
+              <tr>
+                <th>
+                  Recipes
+                </th>
+                <th>
+                </th>
+              </tr>
+            </thead>
+            <tbody>
+              {recipes.map(recipes => (
+                <tr key={recipes.id}>
+                  <td>
+                    <Link to={`recipes/${recipes.id}`} target='_blank'>
+                      <img src={recipes.image} />
+                      <div>
+                        {recipes.name}
+                      </div>
+                    </Link>
+                  </td>
+                  <td>
+                    <Link to={`recipes/${recipes.id}`} target='_blank'>
+                      <button>
+                        <i className="fas fa-external-link-alt"></i>
+                      </button>
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+            <tfoot>
+            <tr>
+              <th>
+               Recipes
+              </th>
+              <th>
+              </th>
+            </tr>
+            </tfoot>
+          </table>
+        </section>
+      </main>
+    </>
   );
 }
 
+//Export Function
 export default Recipes;
