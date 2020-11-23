4d146b84162ae92d0acd8d74f65ba960860fc7d1
Krisztina Pap <krisztina.pap@outlook.com>
2020-11-23 13:21:33 -0700

    [F] build: Adds basic calendar and drag-and-drop functionality. All desktop/month-view at the moment.

--- frontend/src/components/Dashboard.js
+++ frontend/src/components/Dashboard.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import MyCalendar from './MyCalendar';
 
 const Dashboard = () => {
   return (
-    <h1>Dashboard page</h1>
-  );
+    <>
+      <h1 className="mt-6">Dashboard</h1>
+      <MyCalendar />
+    </>
+   );
 }
 
 export default Dashboard;
---
+++ frontend/src/components/MyCalendar.js
@@ -0,0 +1,66 @@
+import React, { Component } from "react";
+import { Calendar, momentLocalizer } from "react-big-calendar";
+import moment from "moment";
+import withDragAndDrop from "react-big-calendar/lib/addons/dragAndDrop";
+import "react-big-calendar/lib/addons/dragAndDrop/styles.css";
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+// Citation: https://www.npmjs.com/package/react-big-calendar
+// Citation: https://github.com/arecvlohe/rbc-with-dnd-starter/blob/master/src/App.js
+
+
+
+//const MyCalendar = () => (
+
+
+//);
+
+//export default MyCalendar;
+
+const localizer = momentLocalizer(moment);
+const DnDCalendar = withDragAndDrop(Calendar);
+
+class MyCalendar extends Component {
+  state = {
+    events: [
+      {
+        start: moment().toDate(),
+        end: moment().add(1, "days").toDate(),
+        title: "Some title",
+      },
+    ],
+  };
+
+  onEventResize = (data) => {
+    const { start, end } = data;
+
+    this.setState((state) => {
+      state.events[0].start = start;
+      state.events[0].end = end;
+      return { events: [...state.events] };
+    });
+  };
+
+  onEventDrop = (data) => {
+    console.log(data);
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <DnDCalendar
+          defaultDate={moment().toDate()}
+          defaultView="month"
+          events={this.state.events}
+          localizer={localizer}
+          onEventDrop={this.onEventDrop}
+          onEventResize={this.onEventResize}
+          resizable
+          style={{ height: "100vh" }}
+        />
+      </div>
+    );
+  }
+}
+
+export default MyCalendar;
