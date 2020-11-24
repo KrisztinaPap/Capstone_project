35bd0c516aa107295409803a6531b0fe301e8c95
Kenji <kenji.au9@gmail.com>
2020-11-24 14:46:11 -0700

    [B] Refactor Changed User model to inherit from IdentityUser;

--- backend/Models/User.cs
+++ backend/Models/User.cs
@@ -7,37 +7,16 @@ using Microsoft.Extensions.Logging;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using FluentValidation;
+using Microsoft.AspNetCore.Identity;
 
 namespace Api.Models
 {
 
   [Table("Users")]
-  public class User {
-
-    [Key]
-    [Column(TypeName = "int(10)")]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
-    
-    [Required]
-    [Column(TypeName = "varchar(50)")]
-    public string Name { get; set; }
-    
-    [Required]    
-    [Column(TypeName = "varchar(50)")]
-    public string Password { get; set; }
-    
-    [Required]    
-    [Column(TypeName = "varchar(50)")]
-    public string PasswordSalt { get; set; }
-
-    [Required] 
-    [Column(TypeName = "varchar(50)")]
-    public string Email { get; set; }
-
+  public class User : IdentityUser
+  {
     public User()
     {
     }
-
   }
 }
