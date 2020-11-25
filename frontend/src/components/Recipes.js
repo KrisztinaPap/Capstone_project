c260fb6ce9ed09466a9959e2b63dfee779c164e9
L Graham <lgraham92@gmail.com>
2020-11-25 11:18:56 -0700

    [Feat] Added Error Handling on Axios Timeout

--- frontend/src/components/Recipes.js
+++ frontend/src/components/Recipes.js
@@ -6,16 +6,23 @@ import axios from 'axios';
 function Recipes() {
 
   const [recipes, setRecipes] = useState([]);
-  const [loading, setLoading] = useState([true]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false)
 
   useEffect(() => {
     populateRecipes();
-  }, [loading]);
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
 
-  async function populateRecipes(){
-    const response = await axios.get('api/recipes')
-    setRecipes(response.data);
-    setLoading(false);
+  async function populateRecipes() {
+    try {
+      const response = await axios.get('api/recipes')
+      setRecipes(response.data);
+      setLoading(false);
+      setError(false);
+    } catch (err) {
+      setError(true);
+      setLoading(false);
+    }
   }
 
   {/* TODO: Design better loading display. Perhaps a loading gif of some sort? Remove center after CSS applied */}
@@ -28,6 +35,19 @@ function Recipes() {
         </center>
       </>
     );
+  }
+
+  // If Axios request has an error, display error message...
+  // TODO: Design better Error page?
+  if (error){
+    return(
+      <>
+        <p>There was an error loading the Recipes List. Please try again.</p>
+        <p><button className="bg-purple-500 hover:bg-purple-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline shadow" type="submit" onClick={populateRecipes}>
+          Retry
+        </button></p>
+      </>
+    )
   }
 
   return (
