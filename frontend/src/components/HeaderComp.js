f8510ca133f9f6109ef385830a780286dc27e57d
L Graham <lgraham92@gmail.com>
2020-11-30 12:33:29 -0700

    [Feat] Add new Header component; Fill header component with placeholder text; WIP - Banner will replace

---
+++ frontend/src/components/HeaderComp.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const HeaderComp = () => {
+  return (
+    <>
+      <center>
+        <section>
+          <p>By: AB, KA, KP, LG, TO</p>
+          <p>This entire section will be replaced with a banner once created</p>
+          <p>Placeholder text added to "mimic" the size of a banner</p>
+        </section>
+      </center>
+    </>
+  );
+}
+
+export default HeaderComp;
+
+
--- frontend/src/components/Layout.js
+++ frontend/src/components/Layout.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { NavMenu } from './NavMenu';
 import { Footer } from './Footer';
+import HeaderComp from './HeaderComp';
 
 export class Layout extends Component {
   static displayName = Layout.name;
@@ -11,6 +12,7 @@ export class Layout extends Component {
         <div id="outer-container" style={{height: '100%'}}>
           <NavMenu />
           <div id="page-wrap">
+            <HeaderComp />
             <div className="flex flex-col h-screen justify-between">
               <div className="container mx-auto mb-auto content-start">
                 {this.props.children}
