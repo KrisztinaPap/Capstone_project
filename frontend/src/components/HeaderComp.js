81e61c682a9bcab5a96daccbbec6f1965d4a3cb5
L Graham <lgraham92@gmail.com>
2020-12-02 12:29:40 -0700

    [F]Removed Comment

--- frontend/src/components/HeaderComp.js
+++ frontend/src/components/HeaderComp.js
@@ -4,13 +4,10 @@ import banner from '../siteimages/NEWYUM1.png'
 const HeaderComp = () => {
   return (
     <>
-      {/* TODO: Replace Below with a Banner once created */}
-      <center>
         <section>
           <div className="w-full h-1 border-top box-shadow bg-purple-500" />
           <img src={banner} alt="test logo" />
         </section>
-      </center>
     </>
   );
 }
