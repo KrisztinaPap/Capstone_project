a3491852991977d21021b007f7aeaf5bf20509dd
L Graham <lgraham92@gmail.com>
2020-12-02 15:17:08 -0700

    [B]Fix banner url

--- frontend/src/components/HeaderComp.js
+++ frontend/src/components/HeaderComp.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import banner from '../../public/yummy-banner.png'
 
 const HeaderComp = () => {
   return (
     <>
         <section>
           <div className="w-full h-1 border-top box-shadow bg-purple-500" />
-          <img src={banner} alt="test logo" className="mx-auto"/>
+          <img src={"/yummy-banner.png"} alt="test logo" className="mx-auto"/>
         </section>
     </>
   );
