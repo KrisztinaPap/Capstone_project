10a9bcd9888b39f87c1d29c1bf7fe685868c0dbd
Krisztina Pap <krisztina.pap@outlook.com>
2020-12-03 10:41:02 -0700

    [F]build: Built pages for 404 and 500 errors. Set catch-all not found page in router.

---
+++ frontend/src/components/Page404.js
@@ -0,0 +1,12 @@
+// Import Resources
+import React from 'react';
+
+const Page404 = () => {
+  return (
+    <div className="flex flex-col justify-center text-center mt-4">
+      <h2 className="text-6xl text-bold text-purple-500">Error 404</h2>
+      <p className="text-xl mt-2">Sorry, the page you requested doesn't exist!</p>
+    </div>
+  )};
+
+export default Page404;
