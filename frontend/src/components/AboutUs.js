b1f9976f4f1b88f0bd23eec8a1c8b23be7b11aaf
L Graham <lgraham92@gmail.com>
2020-12-03 11:13:22 -0700

    [F]Add Aaron Picture

--- frontend/src/components/AboutUs.js
+++ frontend/src/components/AboutUs.js
@@ -13,7 +13,7 @@ const AboutUs = () => {
             <div className="p-4 lg:w-1/4 md:w-1/2">
               <div className="h-full flex flex-col items-center text-center">
                 <img alt="team" className="flex-shrink-0 rounded-lg w-full h-56 object-cover object-center mb-4"
-                     src="https://dummyimage.com/200x200"/>
+                     src="/Aaron.png"/>
                 <div className="w-full">
                   <h2 className="title-font font-medium text-lg text-gray-900">Aaron Barthel</h2>
                   <h3 className="text-gray-500 mb-3">Full Stack Developer</h3>
