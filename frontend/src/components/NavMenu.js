3d7149a39cea27518a5c0622dcf62ced0bed05af
L Graham <lgraham92@gmail.com>
2020-12-04 12:27:37 -0700

    [Bug]Fix consistency with Nav menu sometimes displaying multiple items on one line

--- frontend/src/components/NavMenu.js
+++ frontend/src/components/NavMenu.js
@@ -23,23 +23,35 @@ function NavMenu() {
   const LoggedInMenu = () => {
     return (
       <>
-        <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/dashboard">
-          Dashboard
-        </Link>
-        <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/recipes">
-          Recipes
-        </Link>
-        <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/add-recipe">
-          Add a Recipe
-        </Link>
-        <hr className="mb-2"/>
-        <p>You are logged in as: {user.name}</p>
-        <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/profile">
-          Edit Profile
-        </Link>
-        <button className="m-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-purple-500 hover:bg-purple-700 text-white py-1 px-4 rounded" onClick={LogOut}>
-          Log Out
-        </button>
+        <div>
+          <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/dashboard">
+            Dashboard
+          </Link>
+        </div>
+        <div>
+          <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/recipes">
+            Recipes
+          </Link>
+        </div>
+        <div>
+          <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/add-recipe">
+            Add a Recipe
+          </Link>
+        </div>
+        <div>
+          <hr className="mb-2"/>
+          <p>You are logged in as: {user.name}</p>
+          </div>
+        <div>
+          <Link className="white-link hover:bg-purple-700 hover:font-bold focus:outline-none focus:shadow-outline" to="/profile">
+            Edit Profile
+          </Link>
+        </div>
+        <div>
+          <button className="m-2 transition duration-300 ease-in-out focus:outline-none focus:shadow-outline bg-purple-500 hover:bg-purple-700 text-white py-1 px-4 rounded" onClick={LogOut}>
+            Log Out
+          </button>
+        </div>
       </>
     );
   }
